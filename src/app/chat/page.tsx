ad1740b0304b9eafdff0d0a7ad1a19e4ee16bf64
Rova Fitia <rovafiti@gmail.com>
2025-09-30 13:54:00 +0300

    feat: add Clerk authentication integration and update layout with navigation

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -51,7 +51,7 @@ const RAGChatBot = () => {
     }
  
     return (
-        <div className='max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh)]'>
+        <div className='max-w-4xl mx-auto p-6 relative size-full h-[calc(100vh-4rem)]'>
             <div className="flex flex-col h-full">
                 <Conversation className="h-full">
                     <ConversationContent>
